3dc6da668a048590075a81d24bf9f2bc2f14722f
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-15 12:41:44 +0100

    4/5 done!

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 
 // Impostiamo ora uno script “start” in package.json e facciamo in modo di lanciare il nostro script con npm run start
 
-// Impostiamo un nuovo script “watch” in package.json che possa essere lanciato con npm run dev e che aggiorni in tempo reale le modifiche ai nostri file. Lanciamolo e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. Dovremmo vedere il terminale senza fermare e rilanciare il server.
+// Impostiamo un nuovo script “watch” in package.json che possa essere lanciato con npm run dev e che aggiorni in tempo reale le modifiche ai nostri file.
+// Lanciamolo e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. Dovremmo vedere il terminale senza fermare e rilanciare il server.
 
 // Proviamo a passare da terminale una parola come argomento. da mostrare dopo “Hello”. Dovremo quindi stampare “Hello parola_passata”
 
@@ -13,4 +14,4 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //eseguo in console con node (nomefile): In questo caso node index.js
-console.log("Hello world");
+console.log("Hello World");
